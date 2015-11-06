e05b8147038c28deb7499799f3de8d09eb2e3d3b
David Molineus <david.molineus@netzmacht.de>
2015-11-06 15:34:05 +0100

    Remove superfluous authors.

--- tests/ServiceTest.php
+++ tests/ServiceTest.php
@@ -6,8 +6,7 @@
  * Copyright (C) 2013 Contao Community Alliance
  *
  * @package contao-composer
- * @author  Dominik Zogg <dominik.zogg@gmail.com>
- * @author  Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author  David Molineus <david.molineus@netzmacht.de>
  * @author  Tristan Lins <tristan.lins@bit3.de>
  * @link    http://c-c-a.org
  * @license LGPL-3.0+
